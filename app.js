aa3fca02f941a6d560241cabbceb9165b5c2865e
Irakli <koregm@gmail.com>
2022-02-01 14:20:24 +0400

    Connect to mongo database

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+
+// // Middleware
+// app.use('/post', () => {
+//     console.log('This is a middleware running')
+// })
 
 // Routes
 app.get('/', (req, res) => {
     res.send("We are on home page")
+})
+
+app.get('/post', (req, res) => {
+    res.send("We are on post page")
+})
+
+
+mongoose.connect(process.env.DB_CONNECTION, () => {
+    console.log("Connect to DB")
 })
 
 // Listening to the server
