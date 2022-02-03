f636607f7ba3bb6455fee6a43ffd47b571a799d1
Irakli <koregm@gmail.com>
2022-02-03 20:02:06 +0400

    Separate validation with own file

--- routes/auth.js
+++ routes/auth.js
@@ -6,17 +6,12 @@ const router = express.Router();
 const User = require("../models/User");
 
 // VALIDATION with Joi
-const Joi = require("joi");
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
+const { registerValidation, loginValidation } = require("../validation");
 
 /* Register new user */
 router.post("/register", async (req, res) => {
   // Validate data with Joi, before create User
-  const { error } = schema.validate(req.body);
+  const { error } = registerValidation(req.body);
   // in error case stop code execution and see error on response
   if (error) {
     return res.status(400).send(error.details[0].message);
