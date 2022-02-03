ba3472a7acb51c98981296063330d6f2ddd53279
Irakli <koregm@gmail.com>
2022-02-03 19:50:40 +0400

    Add (Joi) validation on User create.

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,23 @@ const router = express.Router();
 // import User model to save data to database
 const User = require("../models/User");
 
+// VALIDATION with Joi
+const Joi = require("joi");
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
+
 /* Register new user */
 router.post("/register", async (req, res) => {
+  // Validate data with Joi, before create User
+  const { error } = schema.validate(req.body);
+  // in error case stop code execution and see error on response
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
   // prepare data
   const user = new User({
     name: req.body.name,
@@ -14,7 +29,7 @@ router.post("/register", async (req, res) => {
     password: req.body.password,
   });
   try {
-    // save prepared data
+    // save prepared data to mongo database
     const saveUser = await user.save();
     // see saved data on response
     res.json(saveUser);
