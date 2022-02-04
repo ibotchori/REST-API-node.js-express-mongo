3f463e6ed4ea6c8e20e006a5035d6c9eefc89ee0
Irakli <koregm@gmail.com>
2022-02-04 11:12:44 +0400

    Add login validation

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ router.post("/register", async (req, res) => {
   }
 
   // Check, if the user is already in the database send the message
-  emailExist = await User.findOne({ email: req.body.email });
+  const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send("Email already exists");
 
   // Hash (encrypt) the password
@@ -35,12 +35,35 @@ router.post("/register", async (req, res) => {
   try {
     // save user to database
     const saveUser = await user.save();
-    // see saved user on response
-    res.json(saveUser);
+    // see saved user ID on response
+    res.send({ userId: user._id });
   } catch (error) {
     // see error on response
     res.json({ message: error });
   }
+});
+
+/* Login */
+router.post("/login", async (req, res) => {
+  // Validate data with Joi, before create User
+  const { error } = loginValidation(req.body);
+  // in error case stop code execution and see error on response
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // Check, if the user is already in the database
+  const user = await User.findOne({ email: req.body.email });
+  // if email does not exists sent the message
+  if (!user) return res.status(400).send("Email is not found");
+
+  // compare entered password to password from database
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  // if password is invalid send the message on response
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  // if everything is ok, send the message
+  res.send("Logged in");
 });
 
 module.exports = router;
